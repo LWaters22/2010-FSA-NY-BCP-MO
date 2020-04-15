a7ae3010125da30f3fcaf9305c36b96556f51db8
Ariel Ahdoot <aaahdoot@gmail.com>
2020-04-15 00:14:32 -0400

    adding preliminary array I notes

---
+++ 05-arrays-I/J-array-unshift.js
@@ -0,0 +1,14 @@
+/* .unshift adds one or more elements to the front of the array */
+
+let names = ['George', 'John', 'Thomas'];
+
+let newLength = names.unshift('King George III');
+
+console.log(names);
+console.log(newLength);
+
+/*
+Notes:
+--------
+
+*/
