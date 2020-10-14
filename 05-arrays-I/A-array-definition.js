f89a95673f20526aaf1753f8c9635182ea98c559
LWaters22 <laura.waters22@gmail.com>
2020-10-14 18:34:47 -0400

    add arrays

---
+++ 05-arrays-I/A-array-definition.js
@@ -0,0 +1,22 @@
+// What is an array?
+// An array is a list-like data structure in JavaScript 
+
+// array of numbers
+let numbers = [1, 2, 3];
+
+// array of strings
+let names = ['George', 'John', 'Thomas'];
+
+// a variable named aVariable
+let aVariable = 'a value';
+
+// an array containing a number, a string, a null value, and the variable named aVariable
+let mixedBag = [30, true, 'apples', null, aVariable];
+
+// the values inside of an array are called elements
+
+/*
+Notes:
+--------
+
+*/
