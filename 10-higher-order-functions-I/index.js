507069870892f7e9bef2f3053def89ae7c9422c2
LWaters22 <laura.waters22@gmail.com>
2020-10-23 01:43:14 +0000

    notes from lecture

--- 10-higher-order-functions-I/index.js
+++ 10-higher-order-functions-I/index.js
@@ -24,3 +24,24 @@ Corpus from Trump’s victory speech and Clinton's concession speech (note the s
 Corpus from a poem by Pablo Neruda; outputted poems tend to be quite angsty and occasionally romantic (https://repl.it/@tunguska_lore/poemMarkhovChain2)
 Corpus from a scathing review of Guy Fieri’s restaurant in NYC (https://repl.it/@seanbert4/review-of-Guy-Fieris-this-tv-chef-restaurant)
 */
+
+/**
+ * when you get STUCK, 
+ * if your code is not working and you don't know Why
+ * OR if it's working and you don't know Why
+ * 
+ * 1) stop coding
+ * 2) think about what you're trying to do.
+ * 
+ * what datatype(s) do you have?
+ * how have you manipulated those datatypes in the past?
+ * 
+ * each datatype has a specific purpose, it's worth thinking about that purpose
+ */
+
+
+let bridges = ['Brooklyn', 'Golden Gate', 'London'];
+
+let bridges.forEach(function (elem) {
+
+})
